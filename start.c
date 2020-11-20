91194101d692085993739e896af8f9c263837de6
Julien_Deveaux <julien.deveaux@hotmail.com>
2020-11-20 17:07:53 +0100

    fini

--- start.c
+++ start.c
@@ -1,35 +1,36 @@
-#include <stdio.h>          /* printf()                 */
-#include <stdlib.h>         /* exit(), malloc(), free(), random() */
-#include <sys/types.h>      /* key_t, sem_t, pid_t      */
-#include <sys/shm.h>        /* shmat(), IPC_RMID        */
-#include <errno.h>          /* errno, ECHILD            */
-#include <semaphore.h>      /* sem_open(), sem_destroy(), sem_wait().. */
-#include <fcntl.h>          /* O_CREAT, O_EXEC          */
-#include <time.h>			/* time()					*/
-#include <unistd.h>			/* fork()					*/
-#include <sys/wait.h>		/* waitpid()				*/
-#define RANGE 1000000		/* max 1000000 				*/
+/* Projet réalisé en binome Julien Deveaux et Justine Lemesle */
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <errno.h>
+#include <semaphore.h>
+#include <fcntl.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#define RANGE 100    /* max 1000000 */
 
 int main (int argc, char **argv){
-    int i;                        /*      variable de boucle      */
-    key_t shmkey;                 /*      shared memory key       */
-    int shmid;                    /*      shared memory id        */
-    sem_t *sem;                   /*      sync semaphore          *//*shared */
-    pid_t pid;                    /*      fork pid                */
-    int *p;              		  /*      variable partagee       *//*shared */
-    unsigned int n;               /*      bombre de fork          */
-    unsigned int value;           /*      valeur semaphore        */
+    int i;
+    key_t shmkey;
+    int shmid;
+    sem_t *sem;
+    pid_t pid;
+    int *p;
+    unsigned int n;
+    unsigned int value;
 
     /* initialise une variable partagee dans une shared memory */
-    shmkey = ftok ("/dev/null", 5);       /* chemin valide et un chiffre */
+    shmkey = ftok ("/dev/null", 5);
     printf ("shmkey pour p = %d\n", shmkey);
     shmid = shmget (shmkey, sizeof(int)*RANGE, IPC_CREAT | 0666);
-    if (shmid < 0){                           /* shared memory error check */
+    if (shmid < 0){
         perror ("shmget\n");
         exit (1);
     }
 
-    p = (int *)shmat (shmid, NULL, 0);   /* raccord de p à la shared memory */
+    p = (int *)shmat (shmid, NULL, 0);
     printf ("p est en mémoire partagée.\n\n");
 
     /********************************************************/
@@ -40,7 +41,6 @@ int main (int argc, char **argv){
 
     /* initialisation des semaphores pour les shared processes */
     sem = sem_open ("pSem", O_CREAT | O_EXCL, 0644, n); 
-    /* nom du semaphore "pSem" */
 
     printf ("semaphore initialisé\n\n");
 
@@ -49,15 +49,12 @@ int main (int argc, char **argv){
     for (i = 0; i < n; i++){
         pid = fork ();
         if (pid < 0) {
-        /* check si il y a des erreurs */
             sem_unlink ("pSem");   
-            sem_close(sem);  
-            /* unlink evite que le semaphore exit en boucle */
-            /* Si un crash arrive à l'execution             */
+            sem_close(sem);
             printf ("Fork error.\n");
         }
         else if (pid == 0)
-            break;                  /* processus fils */
+            break;
     }
 
 
@@ -65,7 +62,6 @@ int main (int argc, char **argv){
     /******************   PROCESSUS PARENT   ***************/
     /*******************************************************/
     if (pid != 0) {
-        /* on attend que les processus fils terminent tous */
         while (pid = waitpid (-1, NULL, 0)){
             if (errno == ECHILD)
                 break;
@@ -92,7 +88,6 @@ int main (int argc, char **argv){
         printf("Max : %d Min : %d\n", max, min);
         printf("\033[0m");
 
-		/* calcul de la moyenne et de l'espérance */
 		long long unsigned int nbVal, moy, esp, temp;
 		nbVal = 0;
 		moy = 0;
@@ -121,15 +116,11 @@ int main (int argc, char **argv){
 		printf("\033[0m");
 
 
-        /* détachement shared memory */
         shmdt  (p);
         shmctl (shmid, IPC_RMID, 0);
 
-        /* cleanup semaphores */
         sem_unlink ("pSem");   
-        sem_close(sem);  
-        /* unlink evite que le semaphore exit en boucle */
-        /* Si un crash arrive à l'execution             */
+        sem_close(sem);
         exit (0);
     }
 
@@ -144,14 +135,14 @@ int main (int argc, char **argv){
         for(i = 0; i < RANGE; i++) {
 			valeurs[i] = (rand() % RANGE);
 		}
-        sem_wait (sem);           /* Verouille le semaphore */
+        sem_wait (sem);
         printf("\033[1;31m");
         printf ("  Processus(%d) dans la section critique.\n", pid);
         printf("\033[0m");
         for(i = 0; i < RANGE; i++) {
-        	p[(int)valeurs[i]]++; /*On incrémente le tableau partagé a chaque occurence d'une valeur */
+        	p[(int)valeurs[i]]++;
         }
-        sem_post (sem);           /* Libère le semaphore */
+        sem_post (sem);
         printf("\033[1;31m");
         printf("  Processus %d fini\n", pid);
         printf("\033[0m");
